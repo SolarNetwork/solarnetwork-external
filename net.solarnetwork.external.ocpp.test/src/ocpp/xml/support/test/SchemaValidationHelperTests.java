e92491cf06b8917814b47f39f346c5031e15dc4d
Matt Magoffin <matt@solarnetwork.net>
2024-06-21 07:16:26 +1200

    Remove OCPP 1.6 and 2.0 classes that moved to new projects.

--- net.solarnetwork.external.ocpp.test/src/ocpp/xml/support/test/SchemaValidationHelperTests.java
+++ net.solarnetwork.external.ocpp.test/src/ocpp/xml/support/test/SchemaValidationHelperTests.java
@@ -1,21 +1,21 @@
 /* ==================================================================
  * SchemaValidationHelperTests.java - 3/02/2020 2:13:19 pm
- * 
+ *
  * Copyright 2020 SolarNetwork.net Dev Team
- * 
- * This program is free software; you can redistribute it and/or 
- * modify it under the terms of the GNU General Public License as 
- * published by the Free Software Foundation; either version 2 of 
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
  * the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License 
- * along with this program; if not, write to the Free Software 
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
  * 02111-1307 USA
  * ==================================================================
  */
@@ -29,23 +29,24 @@ import javax.xml.bind.JAXBElement;
 import javax.xml.validation.Schema;
 import org.junit.Test;
 import ocpp.domain.SchemaValidationException;
-import ocpp.v16.cp.ChargePointService;
-import ocpp.v16.cs.BootNotificationRequest;
-import ocpp.v16.cs.CentralSystemService;
-import ocpp.v16.cs.ObjectFactory;
+import ocpp.v15.cp.ChargePointService;
+import ocpp.v15.cs.BootNotificationRequest;
+import ocpp.v15.cs.CentralSystemService;
+import ocpp.v15.cs.ObjectFactory;
 import ocpp.xml.support.SchemaValidationHelper;
 
 /**
  * Test cases for the {@link SchemaValidationHelper} class.
- * 
+ *
  * @author matt
- * @version 1.0
+ * @version 2.0
  */
 public class SchemaValidationHelperTests {
 
 	@Test
 	public void extractChargePointSchemaFromWsdl() {
-		Schema s = SchemaValidationHelper.schemaFromWsdl("ocpp/v16/cp/OCPP_ChargePointService_1.6.wsdl",
+		Schema s = SchemaValidationHelper.schemaFromWsdl(
+				"ocpp/v15/cp/ocpp_chargepointservice_1.5_final.wsdl",
 				ChargePointService.class.getClassLoader());
 		assertThat("Schema extracted", s, notNullValue());
 	}
@@ -54,10 +55,10 @@ public class SchemaValidationHelperTests {
 	public void validateJaxbObjectFromWsdlSchema() throws Exception {
 		// given
 		Schema s = SchemaValidationHelper.schemaFromWsdl(
-				"ocpp/v16/cs/OCPP_CentralSystemService_1.6.wsdl",
+				"ocpp/v15/cs/ocpp_centralsystemservice_1.5_final.wsdl",
 				CentralSystemService.class.getClassLoader());
 		SchemaValidationHelper helper = new SchemaValidationHelper(s);
-		JAXBContext ctx = JAXBContext.newInstance("ocpp.v16.cs",
+		JAXBContext ctx = JAXBContext.newInstance("ocpp.v15.cs",
 				CentralSystemService.class.getClassLoader());
 
 		// when
@@ -77,10 +78,10 @@ public class SchemaValidationHelperTests {
 	public void validateJaxbObjectFromWsdlSchema_invalid() throws Exception {
 		// given
 		Schema s = SchemaValidationHelper.schemaFromWsdl(
-				"ocpp/v16/cs/OCPP_CentralSystemService_1.6.wsdl",
+				"ocpp/v15/cs/ocpp_centralsystemservice_1.5_final.wsdl",
 				CentralSystemService.class.getClassLoader());
 		SchemaValidationHelper helper = new SchemaValidationHelper(s);
-		JAXBContext ctx = JAXBContext.newInstance("ocpp.v16.cs",
+		JAXBContext ctx = JAXBContext.newInstance("ocpp.v15.cs",
 				CentralSystemService.class.getClassLoader());
 
 		// when
--- net.solarnetwork.external.ocpp/README.md
+++ net.solarnetwork.external.ocpp/README.md
@@ -7,15 +7,3 @@ The 1.5 OCPP classes were generated via the Java 8 `wsimport` command, like this
 
 	wsimport -extension -p ocpp.v15.cs -target 2.2 -d . -Xnocompile -XadditionalHeaders \
 		ocpp_centralsystemservice_1.5_final.wsdl
-
-The 1.6 OCPP classes were generated via the Java 8 `wsimport` command, like this:
-
-	wsimport -extension -p ocpp.v16.cp -target 2.2 -d . -Xnocompile -XadditionalHeaders \
-		OCPP_ChargePointService_1.6.wsdl
-
-	wsimport -extension -p ocpp.v15.cs -target 2.2 -d . -Xnocompile -XadditionalHeaders \
-		OCPP_CentralSystemService_1.6.wsdl
-
-The 2.0 OCPP classes are generated via the Gradle build in `defs/2.0` directory, like this:
-
-	./gradlew generateJsonSchema2DataClass
